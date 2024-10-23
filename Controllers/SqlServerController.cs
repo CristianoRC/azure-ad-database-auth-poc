e777be8ca98809d31787c76f2ddfcac805fc469e
Cristiano Cunha <cristiano.cunha@akadseguros.com.br>
2024-10-23 14:31:45 -0300

    Cria controller do SQL Server

---
+++ Controllers/SqlServerController.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Ad_Poc.Controllers;
+
+[ApiController]
+[Route("/sqlserver")]
+public class SqlServerController : ControllerBase
+{
+    private readonly string _connectionString;
+
+    public SqlServerController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("database")!;
+    }
+    
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> SqlServer()
+    {
+        try
+        {
+            await using var dbConnection = new SqlConnection(_connectionString);
+            const string sql = "SELECT TOP (10) [CustomerID], [FirstName], [LastName], [CompanyName]  FROM [SalesLT].[Customer]";
+            var users = await dbConnection.QueryAsync<User>(sql);
+            return Ok(users);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { message = e.Message, StackTrack = e.StackTrace });
+        }
+    }
+}
