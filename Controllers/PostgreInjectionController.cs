434e0b91c7c5bfa10212584f3c88bacf3fb2b630
Cristiano Cunha <cristiano.cunha@akadseguros.com.br>
2024-10-23 15:36:46 -0300

    Adiciona Exemplo de configuração com Dependency Injection

---
+++ Controllers/PostgreInjectionController.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace Ad_Poc.Controllers;
+
+[ApiController]
+[Route("/postgresql-injection")]
+public class PostgreInjectionController(NpgsqlConnection  dbConnection) : ControllerBase
+{
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> PostgreSql()
+    {
+        try
+        {
+            const string query = "SELECT schemaname, tablename FROM pg_catalog.pg_tables LIMIT 5;";
+            var response = await dbConnection.QueryAsync<dynamic>(query);
+            return Ok(response);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { message = e.Message, StackTrack = e.StackTrace });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,48 @@
-using Microsoft.Data.SqlClient;
-namespace Ad_Poc;
+using Azure.Core;
+using Azure.Identity;
+using Npgsql;
 
+namespace Ad_Poc;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
+
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        //builder.Services.
+
+        //--------- Configuração do EntraId / Ad no PostgreSQL
+        var connectionString = builder.Configuration["postgre:database"]!;
+        var entraIdUser = builder.Configuration["postgre:entraIdUser"]!;
+        var azurePostgreTokenScope = builder.Configuration["postgre:tokenScope"]!;
+        var connectionStringBuilder = new NpgsqlConnectionStringBuilder(connectionString)
+        {
+            Username = entraIdUser
+        };
+
+        builder.Services.AddNpgsqlDataSource(connectionStringBuilder.ToString(), dataSourceBuilder =>
+        {
+            dataSourceBuilder.UsePeriodicPasswordProvider(async (_, cancellationToken) =>
+            {
+                var azureCredential = new DefaultAzureCredential();
+                var tokenScope = new TokenRequestContext([azurePostgreTokenScope]);
+                var tokenResponse = await azureCredential.GetTokenAsync(tokenScope, cancellationToken);
+                return tokenResponse.Token;
+            }, TimeSpan.FromHours(3), TimeSpan.FromSeconds(5));
+        });
         
+        //--------- Fim da Configuração do PostgreSQL
+
         var app = builder.Build();
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-        
+
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
